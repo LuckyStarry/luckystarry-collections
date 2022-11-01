5440e8e2e537b6f877e6ea3cf56a17d5234a2b7b
SUN BO <starry@vip.qq.com>
2022-11-01 16:46:38 +0800

    完善单元测试用例

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 import { expect } from 'chai'
-import DEFAULT, { Enumerable, List } from '../src/index'
+import DEFAULT, { Enumerable, List, Dictionary, ReadOnlyCollection, ReadOnlyList } from '../src/index'
 
 describe('./index.ts', function () {
   it('存在 默认导出', function () {
@@ -19,6 +19,24 @@ describe('./index.ts', function () {
     expect(List).not.null
     expect(List).not.undefined
     expect(typeof List).to.equal('function')
+  })
+
+  it('存在 Dictionary', function () {
+    expect(Dictionary).not.null
+    expect(Dictionary).not.undefined
+    expect(typeof Dictionary).to.equal('function')
+  })
+
+  it('存在 ReadOnlyCollection', function () {
+    expect(ReadOnlyCollection).not.null
+    expect(ReadOnlyCollection).not.undefined
+    expect(typeof ReadOnlyCollection).to.equal('function')
+  })
+
+  it('存在 ReadOnlyList', function () {
+    expect(ReadOnlyList).not.null
+    expect(ReadOnlyList).not.undefined
+    expect(typeof ReadOnlyList).to.equal('function')
   })
 
   it('默认导出 与 Enumerable 为同一个对象', function () {
